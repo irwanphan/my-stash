b4e4a71c6e005954ba7e6f16cf91c00daee9130f
irwanphan <irwanphan@gmail.com>
2021-08-08 18:00:20 +0700

    add component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Item from "./components/Item"
+
 function App() {
   return <div>
     <h1>my stuff</h1>
-    <div className='card'>
-      <h3>item name</h3>
-      <button>release</button>
-    </div>
+    <Item />
+    <Item />
+    <Item />
   </div>;
 }
 
---
+++ src/components/Item.js
@@ -0,0 +1,12 @@
+function Item() {
+	return (
+		<div className='card'>
+			<h3>item name</h3>
+			<div className="action">
+				<button>release</button>
+			</div>
+		</div>
+	);
+}
+
+export default Item;
