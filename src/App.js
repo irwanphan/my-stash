beebe2e156c740db3d0b64e678a3ec1273a113ac
irwanphan <irwanphan@gmail.com>
2021-08-08 17:47:18 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return <div>
+    <h1>my stuff</h1>
+    <div className='card'>
+      <h3>item name</h3>
+      <button>release</button>
+    </div>
+  </div>;
+}
+
+export default App;
