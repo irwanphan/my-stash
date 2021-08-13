f21c0537e7143b28e0d77dcd351604c5fe8f9830
irwanphan <irwanphan@gmail.com>
2021-08-13 16:44:59 +0700

    NewItemForm ui

--- src/components/items/NewItemForm.js
+++ src/components/items/NewItemForm.js
@@ -1,25 +1,32 @@
 import Card from "../ui/Card";
+import classes from "./NewItemForm.module.sass"
 
 function NewItemForm() {
     return <Card>
-        <form>
-            <div className>
-                <label htmlFor="title">item title</label>
-                <input type="text" required id="title" />
-            </div>
-            <div className>
-                <label htmlFor="image">item image</label>
-                <input type="url" required id="image" />
-            </div>
-            <div className>
-                <label htmlFor="description">item description</label>
-                <input type="text" required id="description" />
-            </div>
-            <div className>
-                <label htmlFor="initial_value">item initial value</label>
-                <input type="number" required id="initial_value" />
-            </div>
-        </form>
+        <div>
+            <h3>add new item</h3>
+            <form className={classes.form}>
+                <div className={classes.inputBlock}>
+                    <label htmlFor="title">item title</label>
+                    <input type="text" required id="title" />
+                </div>
+                <div className={classes.inputBlock}>
+                    <label htmlFor="image">item image</label>
+                    <input type="url" required id="image" />
+                </div>
+                <div className={classes.inputBlock}>
+                    <label htmlFor="description">item description</label>
+                    <textarea required id="description"></textarea>
+                </div>
+                <div className={classes.inputBlock}>
+                    <label htmlFor="initial_value">item initial value</label>
+                    <input type="number" required id="initial_value" />
+                </div>
+                <div className={classes.inputBlock}>
+                    <button>add item</button>
+                </div>
+            </form>
+        </div>
     </Card>
 }   
 
