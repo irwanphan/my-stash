ab836c409e7ab4ed47a71e64487679c75b82f3ba
irwanphan <irwanphan@gmail.com>
2021-08-16 13:01:00 +0700

    content set to know if there is nothing in favorites, then show no fav found

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,9 +5,17 @@ import FavoritesContext from "../store/favorites-context";
 function Favorites() {
     const favoritesCtx = useContext(FavoritesContext)
 
+    let content;
+
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>404 favorites not found, add some loves</p>
+    } else {
+        content = <ItemList items={favoritesCtx.favorites}></ItemList>
+    }
+
     return <section>
         <h1>my favorites</h1>
-        <ItemList items={favoritesCtx.favorites}></ItemList>
+        {content}
     </section>
 }
 
